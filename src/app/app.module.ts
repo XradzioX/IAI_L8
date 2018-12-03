d0ea80afc0172db372955ff664f2276b84b25d87
xradziox <31067@student.pwsztar.edu.pl>
2018-12-03 20:11:28 +0100

    angular app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { MapComponent } from './components/map/map.component';
+import { QuizComponent } from './components/quiz/quiz.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavBarComponent,
+    ContactComponent,
+    BlogComponent,
+    MapComponent,
+    QuizComponent
   ],
   imports: [
     BrowserModule
